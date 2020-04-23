cc2bdd4cbea65dc848a9e04e4b78918a0d4483dc
Shubhi Jain <jainshubhi67@gmail.com>
2020-04-23 22:44:44 +0530

    Updated faster and less memory consuming code

--- Medium/10_RottingOranges.java
+++ Medium/10_RottingOranges.java
@@ -1,49 +1,47 @@
-import java.util.*;
-class RottingOranges{
-    public static void main(String[] args) {
-        int [][] board={{2,1,1},{1,1,0},{0,1,1}};
-        System.out.println(CountTime(board));
+class Solution {
+    public static class pair {
+        int i;
+        int j;
+        int level;
+
+        pair(int i, int j, int l) {
+            this.i = i;
+            this.j = j;
+            this.level = l;
+        }
     }
-
-    static int dir[][]={{1,0},{0,1},{-1,0},{0,-1}};
-
-    public static int CountTime(int [][] board){
-        int rowCount=board.length;
-        int colCount=board.length;
-        Queue<Integer> queue = new ArrayDeque();
-        Map<Integer, Integer> depth = new HashMap();
-        for (int r = 0; r < rowCount; ++r){
-            for (int c = 0; c < colCount; ++c){
-                if (board[r][c] == 2) {
-                    int code = r * colCount + c;
-                    queue.add(code);
-                    depth.put(code, 0);
-                }
-            }
-                
-        }
-            
-        int ans = 0;
-        while (!queue.isEmpty()) {
-            int code = queue.remove();
-            int r = code / colCount, c = code % colCount;
-            for (int k = 0; k < 4; ++k) {
-                int nr = r + dir[k][0];
-                int nc = c + dir[k][1];
-                if (0 <= nr && nr < rowCount && 0 <= nc && nc < colCount && board[nr][nc] == 1) {
-                    board[nr][nc] = 2;
-                    int ncode = nr * colCount + nc;
-                    queue.add(ncode);
-                    depth.put(ncode, depth.get(code) + 1);
-                    ans = depth.get(ncode);
+    public static int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+    public int orangesRotting(int[][] rooms) {
+        LinkedList<pair> queue = new LinkedList<>();
+        for (int i = 0; i < rooms.length; i++) {
+            for (int j = 0; j < rooms[0].length; j++) {
+                if (rooms[i][j] == 2) {
+                    queue.addLast(new pair(i, j, 0));
                 }
             }
         }
-        
-        for (int[] row: board)
-            for (int v: row)
-                if (v == 1)
+        int ans=0;
+        while (queue.size() != 0) {
+            pair vertex = queue.removeFirst();
+            for (int d = 0; d < dir.length; d++) {
+                int r = vertex.i + dir[d][0];
+                int c = vertex.j + dir[d][1];
+                if (r >= 0 && c >= 0 && r < rooms.length && c < rooms[0].length && rooms[r][c] == 1) {
+                    queue.addLast(new pair(r, c, vertex.level + 1));
+                    rooms[r][c] = 2;
+                }
+            }
+            if(ans<vertex.level) ans=vertex.level;
+        }
+        for (int i = 0; i < rooms.length; i++) {
+            for (int j = 0; j < rooms[0].length; j++) {
+                if(rooms[i][j]==1){
                     return -1;
+                }
+            }
+        }
         return ans;
+
     }
 }
+
