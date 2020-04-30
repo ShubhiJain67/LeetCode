c2e593d18aa9ba79b80dc74c6f293650ed0dc9c9
Shubhi Jain <jainshubhi67@gmail.com>
2020-04-30 06:40:14 +0530

    50_Easy

---
+++ Simple/48_SmallerNumbersThanCurrent.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(i==j) continue;
+                if(nums[i]>nums[j]){
+                    ans[i]++;
+                }
+            }
+        }
+        return ans;
+    }
+}
