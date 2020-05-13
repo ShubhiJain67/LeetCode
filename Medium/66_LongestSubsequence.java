4d1bae4e24aeb75b1afedcf48698910aab5dfa4f
Shubhi Jain <jainshubhi67@gmail.com>
2020-05-14 00:08:49 +0530

    Removed Extra Elements

--- Medium/66_LongestSubsequence.java
+++ Medium/66_LongestSubsequence.java
@@ -10,14 +10,8 @@ class Solution {
                 if(s1.charAt(i-1) == s2.charAt(j-1))
                     lcs[i][j] = 1 + lcs[i-1][j-1];
                 else
-                    lcs[i][j] = Math.max(lcs[i-1][j],lcs[i][j-1]); // or we have to take max with right element and top element
+                    lcs[i][j] = Math.max(lcs[i-1][j],lcs[i][j-1]);
             }
-        }
-        for(int i = 1 ; i<=m; i++){
-            for(int j = 1 ; j<=n; j++){
-                System.out.print(lcs[i][j]+" ");
-            }
-            System.out.println();
         }
         return lcs[m][n];
     }
