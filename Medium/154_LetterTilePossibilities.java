cc445cf17be79fe0e8696871af37cee7799f7839
Shubhi Jain <jainshubhi67@gmail.com>
2020-05-18 20:48:57 +0530

    Added Better Approach

--- Medium/154_LetterTilePossibilities.java
+++ Medium/154_LetterTilePossibilities.java
@@ -19,3 +19,22 @@ class Solution {
         return ans;
     }
 }
+//Better complexity
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        return count(tiles);   
+    }
+    public int count(String tiles){
+        int count=0;
+        int visited=0;
+       for(int i=0;i<tiles.length();i++){
+           int mask=(1<<(tiles.charAt(i)-'A'));
+           if((visited&mask)==0){
+               visited=visited^mask;
+               String newTiles=tiles.substring(0,i)+tiles.substring(i+1);
+               count+=count(newTiles)+1;
+           }
+       } 
+        return count;
+    }
+}
