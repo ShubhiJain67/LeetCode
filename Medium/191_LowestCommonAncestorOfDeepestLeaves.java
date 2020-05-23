a088031ce5a082b620fa7a75a2a0782f207bd6bf
Shubhi Jain <jainshubhi67@gmail.com>
2020-05-24 00:21:57 +0530

    Updated 191_Medium

--- Medium/191_LowestCommonAncestorOfDeepestLeaves.java
+++ Medium/191_LowestCommonAncestorOfDeepestLeaves.java
@@ -15,43 +15,20 @@
  */
 class Solution {
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        if(root==null) return root;
-        LinkedList<TreeNode> q1=new LinkedList<>();
-        LinkedList<TreeNode> lastLevel=new LinkedList<>();
-        q1.add(root);
-        lastLevel=new LinkedList<>(q1);
-        while(q1.size()!=0){
-            int size=q1.size();
-            while(size-->0){
-                TreeNode node=q1.removeFirst();
-                if(node.left!=null)
-                    q1.addLast(node.left);
-                if(node.right!=null)
-                    q1.addLast(node.right);
-            }
-            if(q1.size()!=0)
-                lastLevel=new LinkedList<>(q1);
-        }
-        TreeNode ans;
-        if(lastLevel.size()==1)
-            ans=lastLevel.removeFirst();
-        else{
-            TreeNode node1=lastLevel.removeFirst();
-            TreeNode node2 = lastLevel.removeFirst();
-            ans=GetAn(node1,node2,root);
-            while(lastLevel.size()!=0)
-                ans=GetAn(ans,lastLevel.removeFirst(),root);
-        }
-        return ans;
+        return helper(root,getDepth(root),1);
     }
-    private TreeNode GetAn(TreeNode node1,TreeNode node2,TreeNode root){
-        if(root==null || root==node1 || root==node2){
-            return root;
-        }
-        TreeNode left=GetAn(node1,node2,root.left);
-        TreeNode right=GetAn(node1,node2,root.right);
-        if(left==null && right==null)
+    private int getDepth(TreeNode root){
+        if(root==null)
+            return 0;
+        return Math.max(getDepth(root.left),getDepth(root.right))+1;
+    }
+    private TreeNode helper(TreeNode root,int depth,int currDepth){
+        if(root==null)
             return null;
+        if(currDepth==depth)
+            return root;
+        TreeNode left=helper(root.left,depth,currDepth+1);
+        TreeNode right=helper(root.right,depth,currDepth+1);
         if(left==null)
             return right;
         if(right==null)
