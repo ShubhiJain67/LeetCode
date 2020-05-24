9aeb5c386cd2bc74a29feea48d3ee39664d6b8b1
Shubhi Jain <jainshubhi67@gmail.com>
2020-05-24 22:14:54 +0530

    Update 13_BinaryTreeRightSideView.java

--- Medium/13_BinaryTreeRightSideView.java
+++ Medium/13_BinaryTreeRightSideView.java
@@ -9,29 +9,26 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> list=new ArrayList<Integer>();
-        List<Integer> temp=new ArrayList<Integer>();
-        Queue<TreeNode> q=new LinkedList<TreeNode>();
-        if(root==null) return list;
-        q.add(root);
-        q.add(null);
-        while(q.size()!=0){
-            TreeNode t=q.remove();
-            while(t!=null){
-                if(t.left!=null)
-                q.add(t.left);
-                if(t.right!=null)
-                q.add(t.right);
-                temp.add(t.val);
-                t=q.remove();
+        if (root == null) return Collections.emptyList();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<Integer> res = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            TreeNode n = queue.getFirst();
+            res.add(n.val);
+            
+            int size = queue.size();
+            while(size-->0) {
+                TreeNode node = queue.removeFirst();
+                if (node.right != null) {
+                    queue.add(node.right);
+                } 
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
             }
-            q.add(null);
-            list.add(temp.get(temp.size()-1));
-            if(q.peek()==null)break;
-            System.out.println(temp);
-            temp=new ArrayList<Integer>();
-            System.out.println(q.size());
         }
-        return list;
+        
+        return res;
     }
 }
