a3fed923f0330f97065fef7f9f24de75890d9279
Shubhi Jain <jainshubhi67@gmail.com>
2020-05-27 00:57:05 +0530

    Delete 105_NRepeatedElementInSize2NArray.java

--- Simple/105_NRepeatedElementInSize2NArray.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int repeatedNTimes(int[] A) {
-        int N=A.length/2;
-        Arrays.sort(A);
-        if(A[N-1]==A[N-2])
-            return A[N-1];
-        return A[N];
-    }
-}
