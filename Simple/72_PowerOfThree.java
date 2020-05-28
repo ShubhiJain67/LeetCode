3d717faf3801ae899685943507df82bea609f6d5
Shubhi Jain <jainshubhi67@gmail.com>
2020-05-28 13:39:22 +0530

    Delete 72_PowerOfThree.java

--- Simple/72_PowerOfThree.java
+++
@@ -1,6 +0,0 @@
-public class Solution {
-    public boolean isPowerOfThree(int n) {
-        System.out.println(Integer.toString(n, 3));
-        return Integer.toString(n, 3).matches("^10*$");
-    }
-}
