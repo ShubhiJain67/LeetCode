6931a81e178df97b43da3edf39bf711b71c5536c
Shubhi Jain <jainshubhi67@gmail.com>
2020-06-13 18:19:01 +0530

    Update 7_MergeKLinkedlist.java

--- Hard/7_MergeKLinkedlist.java
+++ Hard/7_MergeKLinkedlist.java
@@ -3,35 +3,58 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution{
-public static ListNode mergeKLists(ListNode[] lists){
-    return partion(lists,0,lists.length-1);
-}
-
-public static ListNode partion(ListNode[] lists,int s,int e){
-    if(s==e)  return lists[s];
-    if(s<e){
-        int q=(s+e)/2;
-        ListNode l1=partion(lists,s,q);
-        ListNode l2=partion(lists,q+1,e);
-        return merge(l1,l2);
-    }else
-        return null;
-}
-
-//This function is from Merge Two Sorted Lists.
-public static ListNode merge(ListNode l1,ListNode l2){
-    if(l1==null) return l2;
-    if(l2==null) return l1;
-    if(l1.val<l2.val){
-        l1.next=merge(l1.next,l2);
-        return l1;
-    }else{
-        l2.next=merge(l1,l2.next);
-        return l2;
+class Solution {
+    public ListNode mergeKLists(ListNode[] list) {
+        if(list.length==0)
+            return null;
+        return mergeSort(list,0,list.length-1);
     }
-}
+    private ListNode mergeSort(ListNode[] list,int i,int j){
+        if(i==j)
+            return list[i];
+        int mid=(i+j)/2;
+        ListNode l1=mergeSort(list,i,mid);
+        ListNode l2=mergeSort(list,mid+1,j);
+        return merge(l1,l2);
+    }
+    private ListNode merge(ListNode l1, ListNode l2) {
+        if(l1==null)
+            return l2;
+        if(l2==null)
+            return l1;
+        ListNode ans=null;
+        ListNode curr=null;
+        if(l1.val<l2.val){
+            ans=l1;
+            curr=l1;
+            l1=l1.next;
+        }else{
+            ans=l2;
+            curr=l2;
+            l2=l2.next;
+        }
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                curr.next=l1;
+                l1=l1.next;
+                curr=curr.next;
+            }else{
+                curr.next=l2;
+                l2=l2.next;
+                curr=curr.next;
+            }
+        }
+        if(l1!=null)
+            curr.next=l1;
+        if(l2!=null)
+            curr.next=l2;
+        return ans;
+    }
+    
+    
 }
