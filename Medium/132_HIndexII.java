8717a3bb0e09e6d3187e6597847c8ea6afb8a1b4
Shubhi Jain <jainshubhi67@gmail.com>
2020-06-18 13:12:13 +0530

    Updates 132 Last 242

--- Medium/132_HIndexII.java
+++ Medium/132_HIndexII.java
@@ -1,15 +1,17 @@
 class Solution {
-    public int hIndex(int[] citations) {
-        int result = 0;
-           if(citations == null || citations.length == 0){
-             return result;
-           }
-           int n = citations.length;
-           for(int i = 0; i < n; i++){
-             if(citations[i] >= (n - i)){
-                 return (n-i);       
-               }
-            }
-          return result;
+        public int hIndex(int[] citations) {
+        int n = citations.length;
+        int l = 0,r = n-1;
+        int ans = 0;
+        while(l <= r){
+            int m=(l+r)/2;
+            if(citations[m]>=n-m){
+                ans = Math.max(ans,n-m);
+                r = m-1;
+            }else
+                l = m+1;
+        }
+        return ans;
     }
+
 }
