d0cdd9de93aa25408bcffb73d191c63722b72e95
Shubhi Jain <jainshubhi67@gmail.com>
2020-06-30 22:02:49 +0530

    Updates 23  Last 33

--- Hard/23_MaxDotProductOfTwoSubsequences.java
+++ Hard/23_MaxDotProductOfTwoSubsequences.java
@@ -1,21 +1,22 @@
 class Solution {
     public int maxDotProduct(int[] nums1, int[] nums2) {
-        int N = nums1.length, M = nums2.length;
-        int[][] dp = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                dp[i][j] = nums1[i] * nums2[j];
-                if (i > 0 && j > 0 && dp[i - 1][j - 1] > 0) {
-                    dp[i][j] += dp[i - 1][j - 1];
-                }
-                if (i > 0 && dp[i - 1][j] > dp[i][j]) {
-                    dp[i][j] = dp[i - 1][j];
-                }
-                if (j > 0 && dp[i][j - 1] > dp[i][j]) {
-                    dp[i][j] = dp[i][j - 1];
-                }
-            }
-        }
-        return dp[N - 1][M - 1];
+        dp=new int[nums1.length][nums2.length];
+        for(int i[]:dp)
+            Arrays.fill(i,-1);
+        int ret= maxDot(nums1,nums2,0,0);
+        return ret;
+    }
+    int [][] dp;
+    int MAX=Integer.MIN_VALUE;
+    public int maxDot(int [] nums1,int []nums2,int i,int j){
+        if(i==nums1.length || j==nums2.length)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int a=nums1[i]*nums2[j]+maxDot(nums1,nums2,i+1,j+1);
+        int b=maxDot(nums1,nums2,i,j+1);
+        int c=maxDot(nums1,nums2,i+1,j);
+        dp[i][j]=Math.max(a,Math.max(b,c));
+        return dp[i][j];
     }
 }
